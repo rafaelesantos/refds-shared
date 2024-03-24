8573e7b775805a23dc9537ae6fa50552e48e7784
Rafael Escaleira <rafaelescaleira@icloud.com>
2024-03-24 15:46:46 -0400

    Update RefdsModel.swift

--- Sources/RefdsShared/Model/RefdsModel.swift
+++ Sources/RefdsShared/Model/RefdsModel.swift
@@ -3,3 +3,10 @@ import Foundation
 public protocol RefdsModel: Codable, RefdsLogger {
     
 }
+
+public extension RefdsModel {
+    func logger() {
+        let encodable = (self as Encodable).message
+        self.loggerInstance.info(message: encodable)
+    }
+}
