a0c99f6fcac0a6fdb3e684e9ad818f002c9364f0
Rafael Escaleira <rafaelescaleira@icloud.com>
2024-04-09 19:13:17 -0400

    Update RefdsAlert.swift

--- Sources/RefdsShared/Alert/RefdsAlert.swift
+++ Sources/RefdsShared/Alert/RefdsAlert.swift
@@ -1,6 +1,14 @@
 import Foundation
+import SwiftUI
 
-public protocol RefdsAlert {
+public protocol RefdsAlert: Equatable {
+    var id: UUID { get }
+    var icon: (() -> any View)? { get }
     var title: String? { get }
     var message: String? { get }
+}
+
+public extension RefdsAlert {
+    var id: UUID { .init() }
+    var icon: (() -> any View)? { nil }
 }
