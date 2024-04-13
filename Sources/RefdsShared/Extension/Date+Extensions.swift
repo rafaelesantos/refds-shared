3e007d6fa3d6dfe23e2e4bf2ad7846b72196fb0c
Rafael Escaleira <rafaelescaleira@icloud.com>
2024-04-13 14:18:41 -0400

    Add random date

--- Sources/RefdsShared/Extension/Date+Extensions.swift
+++ Sources/RefdsShared/Extension/Date+Extensions.swift
@@ -11,4 +11,14 @@ public extension Date {
         dateFormatter.dateFormat = dateFormat.value
         return dateFormatter.string(from: self)
     }
+    
+    static var random: Date {
+        guard let day = current.asString(withDateFormat: .day).asInt,
+              let month = current.asString(withDateFormat: .custom("MM")).asInt,
+              let year = current.asString(withDateFormat: .year).asInt else { return current }
+        guard let pastDate = "\(day)/\(month - 2)/\(year)".asDate(withFormat: .dayMonthYear)?.timestamp else { return current }
+        let currentDate = current.timestamp
+        let randomDate: TimeInterval = .random(in: pastDate ... currentDate)
+        return randomDate.date
+    }
 }
