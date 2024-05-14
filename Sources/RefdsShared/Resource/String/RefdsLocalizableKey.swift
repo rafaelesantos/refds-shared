0196436eff790f94909f1cf17ac71846a52beebd
Rafael Escaleira <rafaelescaleira@icloud.com>
2024-05-14 14:48:54 -0400

    Create RefdsLocalizableKey.swift

---
+++ Sources/RefdsShared/Resource/String/RefdsLocalizableKey.swift
@@ -0,0 +1,17 @@
+import Foundation
+
+public enum RefdsLocalizableKey: String {
+    case lockScreenTitle
+    case lockScreenDescription
+    case lockScreenButtonTitle
+    case lockScreenRequestAuthReason
+    case lockScreenAuthErrorNoBiometricsTitle
+    case lockScreenAuthErrorNoBiometricsDescription
+    case lockScreenAuthErrorTitle
+    case lockScreenAuthErrorDescription
+    case riskLowDescription
+    case riskMediumDescription
+    case riskHighDescription
+    case riskWarningDescription
+    case welcomeIntroduction
+}
