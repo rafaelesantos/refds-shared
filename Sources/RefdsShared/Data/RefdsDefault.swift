ee7763add4ef71462ab4ba446c5b733b9aabc8dd
Rafael Escaleira <rafaelescaleira@icloud.com>
2024-07-17 12:57:32 -0400

    Update RefdsDefault.swift

--- Sources/RefdsShared/Data/RefdsDefault.swift
+++ Sources/RefdsShared/Data/RefdsDefault.swift
@@ -4,10 +4,21 @@ import Foundation
 public struct RefdsDefault<Value> {
     let key: String
     let defaultValue: Value
-    var defaults: UserDefaults = .standard
     
     public var wrappedValue: Value? {
-        get { defaults.object(forKey: key) as? Value }
-        set { defaults.set(newValue, forKey: key) }
+        get { Self.defaults.object(forKey: key) as? Value }
+        set { Self.defaults.set(newValue, forKey: key) }
+    }
+    
+    static var defaults: UserDefaults {
+        UserDefaults(suiteName: "refds.defaults") ?? .standard
+    }
+    
+    static func get<T>(for key: String) -> T? {
+        defaults.object(forKey: key) as? T
+    }
+    
+    static func set<T>(_ value: T, for key: String) {
+        defaults.set(value, forKey: key)
     }
 }
