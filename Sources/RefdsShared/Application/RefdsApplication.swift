2c13d74ea8615c7f149503075299d32966fce689
Rafael Escaleira <rafaelescaleira@icloud.com>
2024-08-22 15:08:07 -0400

    Update RefdsApplication.swift

--- Sources/RefdsShared/Application/RefdsApplication.swift
+++ Sources/RefdsShared/Application/RefdsApplication.swift
@@ -7,22 +7,15 @@ import UIKit
 
 public final class RefdsApplication {
     public static let shared = RefdsApplication()
-#if os(macOS)
-    public var rootViewController: NSViewController? {
-        if let window = NSApplication.shared.windows.first,
-           let rootViewController = window.contentViewController {
-            return rootViewController
-        }
-        return nil
+    
+    public var rootViewController: RefdsViewController? {
+        #if os(macOS)
+        let window = NSApplication.shared.windows.first
+        return window?.contentViewController?.refdsViewController
+        #else
+        let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene
+        let window = windowScene?.windows.first
+        return window?.rootViewController?.refdsViewController
+        #endif
     }
-#else
-    public var rootViewController: UIViewController? {
-        if let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene,
-           let window = windowScene.windows.first,
-           let rootViewController = window.rootViewController {
-            return rootViewController
-        }
-        return nil
-    }
-#endif
 }
