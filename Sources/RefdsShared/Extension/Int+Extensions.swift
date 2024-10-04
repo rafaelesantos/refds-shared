1125f15fec32ff36a962eda2cbe69957ed32c264
Rafael Escaleira <rafaelescaleira@icloud.com>
2024-10-03 23:59:32 -0400

    Update Int+Extensions.swift

--- Sources/RefdsShared/Extension/Int+Extensions.swift
+++ Sources/RefdsShared/Extension/Int+Extensions.swift
@@ -2,4 +2,18 @@ import Foundation
 
 public extension Int {
     var letter: String { String(format: "%c", self) }
+    
+    var seconds: Int { self }
+    
+    var minutes: Int { seconds * 60 }
+    
+    var hours: Int { minutes * 60 }
+    
+    var days: Int { hours * 24 }
+    
+    var weeks: Int { days * 7 }
+    
+    var months: Int { weeks * 4 }
+    
+    var years: Int { months * 12 }
 }
