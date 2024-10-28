6336120670ab68846077fc25c661ccaeefc6b017
Rafael Escaleira <rafaelescaleira@icloud.com>
2024-10-27 23:11:24 -0400

    Update ArrayExtensions.swift

--- Sources/RefdsShared/Extension/ArrayExtensions.swift
+++ Sources/RefdsShared/Extension/ArrayExtensions.swift
@@ -1,20 +1,18 @@
 import Foundation
 
 public extension Array {
-    func insertionIndexOf(_ elem: Element, isOrderedBefore: (Element, Element) -> Bool) -> Int {
-        var lo = 0
-        var hi = self.count - 1
-        while lo <= hi {
-            let mid = (lo + hi) / 2
-            if isOrderedBefore(self[mid], elem) { lo = mid + 1 }
-            else if isOrderedBefore(elem, self[mid]) { hi = mid - 1 }
-            else { return mid }
-        }
-        return lo
-    }
-    
     subscript(safe index: Int) -> Element? {
         guard index >= 0 && index < count else { return nil }
         return self[index]
+    }
+    
+    mutating func insert<T: Comparable>(
+        _ element: Element,
+        by keyPath: KeyPath<Element, T>
+    ) {
+        let index = self.firstIndex {
+            $0[keyPath: keyPath] >= element[keyPath: keyPath]
+        } ?? self.count
+        self.insert(element, at: index)
     }
 }
