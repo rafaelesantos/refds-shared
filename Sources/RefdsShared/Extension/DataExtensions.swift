62b22c24825c2f2bb9ef368f4b3dcb623097455a
Rafael Escaleira <rafaelescaleira@icloud.com>
2024-10-31 10:07:59 -0400

    Update DataExtensions.swift

--- Sources/RefdsShared/Extension/DataExtensions.swift
+++ Sources/RefdsShared/Extension/DataExtensions.swift
@@ -1,12 +1,15 @@
 import Foundation
 
 public extension Data {
-    func asModel<T: Decodable>(dateFormat: String.DateFormat? = nil) -> T? {
+    func asModel<T: Decodable>(
+        for type: T.Type,
+        with dateFormat: String.DateFormat? = nil
+    ) throws -> T {
         let jsonDecoder = JSONDecoder()
-        guard let dateFormat = dateFormat else { return try? jsonDecoder.decode(T.self, from: self) }
+        guard let dateFormat = dateFormat else { return try jsonDecoder.decode(T.self, from: self) }
         let dateFormatter = DateFormatter()
         dateFormatter.dateFormat = dateFormat.value
         jsonDecoder.dateDecodingStrategy = .formatted(dateFormatter)
-        return try? jsonDecoder.decode(T.self, from: self)
+        return try jsonDecoder.decode(T.self, from: self)
     }
 }
