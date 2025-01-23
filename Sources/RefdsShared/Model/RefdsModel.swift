fddb850f138aca4391690e988f98ab4b531c1499
Rafael Escaleira <rafaelescaleira@icloud.com>
2025-01-22 22:25:53 -0400

    Add extension refds model array

--- Sources/RefdsShared/Model/RefdsModel.swift
+++ Sources/RefdsShared/Model/RefdsModel.swift
@@ -10,3 +10,12 @@ public extension RefdsModel {
         await Self.loggerInstance.info(message: encodable)
     }
 }
+
+extension Array: RefdsModel where Element: RefdsModel {}
+
+extension Array: RefdsLogger where Element: RefdsModel {
+    public func logger() async {
+        let encodable = (self as Encodable).message
+        await Self.loggerInstance.info(message: encodable)
+    }
+}
